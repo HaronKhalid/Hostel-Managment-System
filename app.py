904c643dc4c17ffd5b4ae1d0e0aa0cf337d3ded4
92haroonkhalid <muhammadharoonkhalid786@gmail.com>
2026-06-17 13:37:34 +0500

    Implement multi-page architecture with base template

--- app.py
+++ app.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/facilities.html')
+def facilities():
+    return render_template('facilities.html')
+
+@app.route('/rooms.html')
+def rooms():
+    return render_template('rooms.html')
+
+@app.route('/mess.html')
+def mess():
+    return render_template('mess.html')
+
+@app.route('/gallery.html')
+def gallery():
+    return render_template('gallery.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
